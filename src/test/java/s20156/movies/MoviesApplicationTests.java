37c979ed310d5767674eea6c9e30cec8a2e528a0
Mateusz Grube <s20156@pjwstk.edu.pl>
2021-05-09 14:22:22 +0200

    first commit

---
+++ src/test/java/s20156/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package s20156.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
